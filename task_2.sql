82943414e7d3cbd82e3a8b69469de467327df97f
wealthdevtech <wealthydaniel56@gmail.com>
2025-08-16 13:07:46 +0200

    First commit of task2sql file

---
+++ task_2.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130),
+    author_id INT FOREIGN KEY,
+    price DOUBLE,
+    publication_date DATE 
+);
+
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT FOREIGN KEY,
+    order_date DATE
+);
+
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT FOREIGN KEY,
+    book_id INT FOREIGN KEY,
+    quantity DOUBLE
+);
